a1d614e21db578d3fc2e4b388ef666809af97196
zimberkazmiz <zimberkazmiz@gmail.com>
2019-02-19 13:19:11 +0330

    bugfix in arabic numbers

--- src/Middleware/BaseNumbersMiddleware.php
+++ src/Middleware/BaseNumbersMiddleware.php
@@ -16,22 +16,29 @@ abstract class BaseNumbersMiddleware extends TransformsRequest
     /** @var array */
     protected $farsiNumbers = [
         '۰' => 0,
-        '١' => 1,
-        '٢' => 2,
-        '٣' => 3,
+        '۱' => 1,
+        '۲' => 2,
+        '۳' => 3,
         '۴' => 4,
         '۵' => 5,
         '۶' => 6,
-        '٧' => 7,
-        '٨' => 8,
-        '٩' => 9,
+        '۷' => 7,
+        '۸' => 8,
+        '۹' => 9,
     ];
 
     /** @var array */
     protected $arabicNumbers = [
+        '٠' => 0,
+        '١' => 1,
+        '٢' => 2,
+        '٣' => 3,
         '٤' => 4,
         '٥' => 5,
         '٦' => 6,
+        '٧' => 7,
+        '٨' => 8,
+        '٩' => 9,
     ];
 
     /** @var array|mixed */
